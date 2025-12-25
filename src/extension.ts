5d4e39f22752d021ff098690516226337bfe70ed
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-12-25 01:13:04 +0100

    Basic syntax highlighting

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('flisp-assembly.helloWorld', () => {
+	// const disposable = vscode.commands.registerCommand('flisp-assembly.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Flisp Assembly!');
-	});
+		// vscode.window.showInformationMessage('Hello World from Flisp Assembly!');
+	// });
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
